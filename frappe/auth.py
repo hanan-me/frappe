0cd283cc1832fddda9ef87db2fa61e1db61c543d
hanan-me <142660158+hanan-me@users.noreply.github.com>
2025-02-03 21:43:57 +0500

    Update auth.py

--- frappe/auth.py
+++ frappe/auth.py
@@ -217,27 +217,25 @@ class LoginManager:
 			frappe.local.cookie_manager.set_cookie("system_user", "no")
 			if not resume:
 				frappe.local.response["message"] = "No App"
-				frappe.local.response["home_page"] = get_default_path() or "/" + get_home_page()
+				frappe.local.response["home_page"] = "/" + get_home_page()
 		else:
 			frappe.local.cookie_manager.set_cookie("system_user", "yes")
 			if not resume:
 				frappe.local.response["message"] = "Logged In"
-				frappe.local.response["home_page"] = get_default_path() or "/app"
+				frappe.local.response["home_page"] = "/app"
 
 		if not resume:
 			frappe.response["full_name"] = self.full_name
 
 		# redirect information
-		redirect_to = frappe.cache.hget("redirect_after_login", self.user)
+		redirect_to = frappe.cache().hget("redirect_after_login", self.user)
 		if redirect_to:
 			frappe.local.response["redirect_to"] = redirect_to
-			frappe.cache.hdel("redirect_after_login", self.user)
+			frappe.cache().hdel("redirect_after_login", self.user)
 
 		frappe.local.cookie_manager.set_cookie("full_name", self.full_name)
 		frappe.local.cookie_manager.set_cookie("user_id", self.user)
 		frappe.local.cookie_manager.set_cookie("user_image", self.info.user_image or "")
-		# cache control: round trip the effectively delivered language
-		frappe.local.cookie_manager.set_cookie("user_lang", self.user_lang)
 	def clear_preferred_language(self):
 		frappe.local.cookie_manager.delete_cookie("preferred_language")
 
@@ -264,28 +262,6 @@ class LoginManager:
 			return
 
 		clear_sessions(frappe.session.user, keep_current=True)
-
-	def custom_login(usr, pwd, action=None):
-	    if not usr or not pwd:
-	        frappe.throw(_("Username and password required"), frappe.AuthenticationError)
-	
-	    # Authenticate user
-	    user_doc = frappe.get_doc("User", usr)
-	    if not user_doc:
-	        frappe.throw(_("Invalid user"), frappe.AuthenticationError)
-	
-	    check_password(usr, pwd)  # Validate password
-	
-	    if action == "app-user":
-	        # Fetch API Key and Secret from Custom Doctype
-	        custom_doc = frappe.get_value("App User", {"email": usr}, ["api_key", "api_secret"])
-	        if not custom_doc:
-	            frappe.throw(_("API Key and Secret not found"), frappe.AuthenticationError)
-	        
-	        api_key, api_secret = custom_doc
-	        return {"api_key": api_key, "api_secret": api_secret}
-	
-	    return {"message": "Login successful"}
 	
 	# def authenticate(self, user: str | None = None, pwd: str | None = None):
 	# 	from frappe.core.doctype.user.user import User
